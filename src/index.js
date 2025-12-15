6afcc66f309db790315bf6320ecf8410dbd759ba
OmarKira <omarhimmi2001@gmail.com>
2025-12-15 14:04:58 +0100

    Fix routing for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom'; 
 import store from './store/store';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
-// import './styles/main.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
 root.render(
   <Provider store={store}>
-    <HashRouter>
+    <HashRouter> 
       <App />
-    </HashRouter>  
+    </HashRouter>
   </Provider>
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
